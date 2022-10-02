cddb49c42cb9eadd7a6b8acae047fd59ae88f010
stanmozolevskiy <stanmozolevskiy@gmail.com>
2022-10-02 06:14:32 -0400

    Graphs implamentation

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -49,12 +49,63 @@ class BinarySearchTree{
        
         return false;
     }
+    delete(val) {
+        if (!this.root) return false;
+    
+        let prev = null;
+        let cur = this.root;
+        while (cur) {
+          if (cur.val > val) {
+            prev = cur;
+            cur = cur.left;
+          }
+          else if (cur.val < val) {
+            prev = cur;
+            cur = cur.right;
+          }
+          else if (cur.val === val) {
+            // easy one (both children are null)
+            if (cur.left === null && cur.right === null) {
+              if (prev.left.val === val)
+                prev.left = null;
+              else prev.right = null;
+              return;
+            }
+            // hard one (when one child is null)
+            if (cur.left === null && cur.right != null) {
+              if (prev.left.val === val)
+                prev.left = cur.right;
+              else prev.right = cur.right;
+              return;
+            }
+            //hard one (when one child is null)
+            if (cur.right === null && cur.left != null) {
+              if (prev.left.val === val)
+                prev.left = cur.left;
+              else prev.right = cur.left;
+              return;
+            }
+            // hard one (when both are not null)
+            else{
+               if (prev.left.val === val){
+                 let left = cur.left;
+                 prev.left = cur.right;
+                 prev.left.left = left;
+                 return;
+               }else{
+                   return;
+               }
+            }
+          }
+        }
+      }
+
 }
 
 function traverse(node){
-    const tree = {value: node.value};
-    tree.left = nede.left === null? null: traverse(node.left);
-    tree.right = nede.right === null? null: traverse(node.right);
+    const tree = {val: node.val};
+    tree.left = node.left === null? null: traverse(node.left);
+    tree.right = node.right === null? null: traverse(node.right);
     return tree;
 }
 
@@ -67,6 +118,7 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+tree.insert(160);
 
 console.log(JSON.stringify(traverse(tree.root)))
 
