26aad5c2187c63b61e8286d651b5a71a90b6e8ec
stanmozolevskiy <stanmozolevskiy@gmail.com>
2022-10-09 19:26:17 -0400

    fibon seq exersize

--- Algorithms/Recursion.js
+++ Algorithms/Recursion.js
@@ -1,12 +1,10 @@
 // Write two functions that finds the factorial of any number. 
 // One should use recursive, the other should just use for loop
 
-function findFactorialRecursive(number, answer){
-    if(number === 1) return answer;
+function findFactorialRecursive(number){
+    if(number === 2) return 2;
     
-    answer = answer == 0 ? number * (number -1) : answer * (number -1);
-
-    return findFactorialRecursive(number - 1, answer);
+    return number * findFactorialRecursive(number - 1);
 }
 
 console.log(findFactorialRecursive(4, 0));
@@ -22,3 +20,32 @@ function findFactorialIterative(number){
 }
 
 console.log(findFactorialIterative(5));
+
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+// 0,1,1,2,3,5,8,13,21,34,55,89,144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 -> 2+3
+
+function fibonacciIterative(n){
+    //code here 
+    let first = 0;
+    let second = 1;
+    let third = 1;
+    for(let i = 2; i< n; i++){
+        first = second;
+        second = third;
+        third = first + second;
+    }
+    return third;
+}
+
+console.log(fibonacciIterative(6));
+
+function fibonacciIterativeRecursive(n){
+    if( n === 1 ) return 0;
+    if( n === 2 ) return 1;
+    return fibonacciIterativeRecursive(n-1) + fibonacciIterativeRecursive(n-2);
+}
+
+console.log(fibonacciIterativeRecursive(7));
+
+``
