c9821026a3d4cb6b457b569e399e2216c7d46819
stanmozolevskiy <stanmozolevskiy@gmail.com>
2022-10-11 11:44:26 -0400

    insert sort alg

--- Algorithms/Sorting.js
+++ Algorithms/Sorting.js
@@ -37,5 +37,25 @@ function selectSort(arr){
     return arr;
 }
 
-let arr = [6,5,3,1,8,7,2,4];
 console.log(selectSort(arr));
+
+
+function inssertSort(arr){
+    let j, i, key;
+    for(i = 1; i< arr.length; i++){
+        key = arr[i];
+        j = i-1;
+        /*Move elements of arr[0..i-1], that are
+        greater than key, to one position ahead
+        of their current position */
+        while (j >= 0 && arr[j] > key){
+            arr[j+1]= arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
+}
+
+let arr = [6,5,3,1,8,7,2,4];
+console.log(inssertSort(arr));
