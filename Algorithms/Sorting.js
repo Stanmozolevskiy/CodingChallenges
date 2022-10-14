589645726de52d037bc230b62b683e6cd4ca289c
stanmozolevskiy <stanmozolevskiy@gmail.com>
2022-10-14 16:55:39 -0400

    finishing sorting algorithms

--- Algorithms/Sorting.js
+++ Algorithms/Sorting.js
@@ -93,3 +93,30 @@ function merge(left, right){
 let arr = [6,5,3,1,8,7,2,4];
 console.log(mergeSort(arr));
 
+
+// Questions for the intervieu for sorting
+
+//#1 - Sort 10 schools around your house by distance:
+// Since it is the small number of items to sort I will use the INSERT SORT,
+// it is easy to implement and it performs the best on the small datasets.
+
+//#2 - eBay sorts listings by the current Bid amount:
+// I would use the QUICKSORT or MERGESORT because they are the best sorting algorithms for the large data sets
+
+//#3 - Sport scores on ESPN
+// Depending on the numbers I qould use something faster than regular QS or MS something like Non Comparison Sorting algorithms that check bytes
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// The only acceptable algorithm for this scenario is QS becase it is the best for the large datasets and the best for space complexity
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// INSERT SORT would be the best for almost sorted date and for smalll range of data
+
+//#6 - Temperature Records for the past 50 years in Canada
+// QS would be the best because it has a lot of data and it is outperforms all of the algorithms. Has good space complexity
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// QS would be the best because the random date and large database
+
+//#8 - You want to teach sorting for the first time
+// Bubble sort and Insert sort, supper easy to implement and pretty simple to understand 
